1be102906df62f41ea3426db192b549b4e6b30de
MrCat <2268292872@qq.com>
2022-10-20 16:10:15 +0800

    test01

---
+++ src/main/java/com/git/HelloTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author MrCat
+ * @create 2022-10-20 16:05
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("git的test");
+    }
+}
